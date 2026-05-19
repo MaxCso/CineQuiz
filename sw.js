717e279c9b2b36b8f5d4b60f7404055a75d2da24
MaxCso <104164870+MaxCso@users.noreply.github.com>
2026-05-20 00:22:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'cinequiz-v78';
+const STATIC = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
